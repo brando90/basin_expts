a056f2cbf9c69cd250df3a4e8c1aa4cb6316832e
Brando <brando90@mit.edu>
2017-03-24 19:25:47 -0400

    GDL experiment

--- basin_GDL/GLD_basin.m
+++ basin_GDL/GLD_basin.m
@@ -1,44 +1,50 @@
 clear;
 %% GDL & mdl params
-D = 3;
+D = 2;
 mu1 = 4*ones(1,D);
 std1 = 1.0;
 mu2 = 12.0*ones(1,D);
 std2 = 2.0;
-W = 6.5*ones(1,D) + normrnd(0.0,2.0);
+%W = 6.5*ones(1,D) + normrnd(0.0,2.0);
+W = 6.5*ones(1,D);
+%W = [1.0,3.0]
 %
-iter = 30^3;
+iter = 100*30^2;
 eta = 1.0;
 B = 18;
 %
-A = 1.0;
+A = 0.8;
 mu_noise = 0.0;
 std_noise = 1.0;
 %% histogram
 nbins = 30;
 print_hist = 1;
 W_history = zeros(iter,D);
+g_history = zeros(iter,D);
 filename = 'current_gdl_run'
 %% optimize
-for i=1:iter
-    % 
+W_history(1,:) = W;
+for i=2:iter+1
+    %
     g = get_gradient(W,mu1,std1,mu2,std2);
-    eps = normrnd(mu_noise,std_noise);
+    eps = normrnd(mu_noise,std_noise,[1,D]);
     % 
+    %W = mod(W - eta*g, B);
     W = mod(W - eta*g + A*eps, B);
     %
     W_history(i,:) = W;
-    %
-    %U = U_func( W,mu1,std1,mu2,std2 );
-    %fprintf('U=%s, W=%s \n',U,W)
+    g_history(i,:) = g;
 end
 %%
 save(filename)
-%%
+%W_history
 if print_hist
-    for d=1:D
-        figure;
-        histogram(W_history(:,d),nbins)
+    if D==2
+        hist3(W_history,[nbins,nbins]);
+    else
+        for d=1:D
+            figure;
+            histogram(W_history(:,d),nbins)
+        end
     end
-    W_history
 end
