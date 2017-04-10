13b77e959f214a72299f6a7818ee2f6ac38876e4
Brando Miranda <brandomiranda@dhcp-18-189-5-76.dyn.mit.edu>
2017-04-10 15:15:49 -0400

    rbf for 2D visualization basin looks good

--- wedges_GDL/visualize_rbf_wedge.m
+++ wedges_GDL/visualize_rbf_wedge.m
@@ -12,10 +12,11 @@ offset_i_coord = 8;
 % non-degenerate
 c = [3,3];
 apex = -1;
-lb = 2;
-ub = 4;
+lb_pyramid = 2;
+ub_pyramid = 4;
 %
-f = @(x) high_D_pyramid(x,c,apex,lb,ub) + degenerate_wedge(x - offset_i_coord,i_coord); 
+f = @(x) high_D_pyramid(x,c,apex,lb_pyramid,ub_pyramid) + degenerate_wedge(x - offset_i_coord,i_coord) - 1;
+%f = @(x) high_D_pyramid(x,c,apex,lb_pyramid,ub_pyramid) + degenerate_wedge(x - offset_i_coord,i_coord); 
 % plot wedge function
 Z = get_Z_from_meshgrid_f(X,Y,f);
 figure;
@@ -23,21 +24,27 @@ surf(X,Y,Z);
 ylabel('weight W_1')
 xlabel('weight W_2')
 zlabel('Loss')
-%% RBF Wedge
+%% RBF degenerate Wedge
 [ X_data,Y_data] = make_data_from_meshgrid( X,Y,Z ); % X_data = [N, D], Y_data = [N, D_out]
 % get RBF function
 D = 2;
-K = 10;
+K = 50;
 t = get_centers(K,D,i_coord,offset_i_coord,lb,ub); % K x D
-stddev = sqrt(2);
+%t = [t ; c];
+stddev = abs(2)/4;
 beta = 1/(2*stddev^2);
-%
 Kern = produce_kernel_matrix_bsxfun(X_data, t, beta); % (N x K)
 C = Kern \ Y_data; % (K x 1)
-%
 tt = sum(t.^2,2)';
-f = @(x) exp( -beta*eucledian(x,t,tt) ) * C;
-% plot RBF function
+degenerate_rbf_wedge = @(x) exp( -beta*eucledian(x,t,tt) ) * C;
+%% RBF nondegenerate Wedge
+beta_non = 0.1;
+t_pyramid = c;
+non_degenerate_rbf_pyramid = @(x) -0.3 * exp( -beta * norm(x-t_pyramid,2) );
+%% get loss surface f
+f = @(x) degenerate_rbf_wedge(x) + non_degenerate_rbf_pyramid(x);
+f = @(x) (1/0.3)*f(x);
+%% plot RBF function
 Z_rbf = get_Z_from_meshgrid_f(X,Y,f);
 figure;
 surf(X,Y,Z_rbf);
