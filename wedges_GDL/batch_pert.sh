535a9a1c24894bd5a1d7d8eb4f0f11dfd7724950
Brando Miranda <brando90@mit.edu>
2017-12-11 17:36:13 -0500

    changed batch submisison scrip

--- wedges_GDL/batch_pert.sh
+++ wedges_GDL/batch_pert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=2D_potential
-#SBATCH --nodes=1
-#SBATCH --mem=2000
-#SBATCH --time=0:01:00
+#SBATCH --mem=7000
+#SBATCH --time=0-11:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=brando90@mit.edu
 #SBATCH --array=1-3
 
 #alias matlab='/Applications/MATLAB_R2017a.app/bin/matlab -nodesktop -nosplash'
@@ -10,6 +10,6 @@
 #SLURM_ARRAY_TASK_ID=3
 
 SLURM_JOBID=${SLURM_JOBID}
-SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID:-1}
+SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID}
 print_hist=0
 matlab -nodesktop -nosplash -nojvm -r "run_GDL_wedge_perturbations($SLURM_JOBID,$SLURM_ARRAY_TASK_ID,$print_hist)"
