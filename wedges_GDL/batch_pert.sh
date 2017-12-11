a41a2b8db45e72766d395bf5a5bcb01e9ed9d2fa
Brando Miranda <brando90@mit.edu>
2017-12-11 17:39:26 -0500

    corrected submission script

--- wedges_GDL/batch_pert.sh
+++ wedges_GDL/batch_pert.sh
@@ -5,11 +5,13 @@
 #SBATCH --mail-user=brando90@mit.edu
 #SBATCH --array=1-3
 
-#alias matlab='/Applications/MATLAB_R2017a.app/bin/matlab -nodesktop -nosplash'
-#SLURM_JOBID=0
-#SLURM_ARRAY_TASK_ID=3
+echo start
 
-SLURM_JOBID=${SLURM_JOBID}
-SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID}
+#alias matlab='/Applications/MATLAB_R2017a.app/bin/matlab -nodesktop -nosplash'
+#jid=0
+#satid=3
+
+jid=${SLURM_JOBID}
+satid=${SLURM_ARRAY_TASK_ID}
 print_hist=0
-matlab -nodesktop -nosplash -nojvm -r "run_GDL_wedge_perturbations($SLURM_JOBID,$SLURM_ARRAY_TASK_ID,$print_hist)"
+matlab -nodesktop -nosplash -nojvm -r "run_GDL_wedge_perturbations($jid,$satid,$print_hist)"
